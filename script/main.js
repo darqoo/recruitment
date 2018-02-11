fc3a491c4155c094742f7e4705614692add3332a
Darqoo <d.smolag@gmail.com>
2018-02-11 21:43:30 +0100

    added news section

--- script/main.js
+++ script/main.js
@@ -1,9 +1,42 @@
 $(document).ready(function() {
-  
-    $('.carousel').carousel({
+
+  (function() {
+      $(document).ready(function() {
+          $(function() {
+              $(window).scroll(function() {
+                  if ($(this).scrollTop() > 200) {
+                    $(".arrow").css('transform','rotate(180deg)');
+                    $('#ar_a').attr('href', '#');
+                  } else {
+                    $(".arrow").css('transform','rotate(0deg)');
+                    $('#ar_a').attr('href', '#footer');
+                  }
+              });
+          });
+
+      });
+  }());
+
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+                window.scrollTo(0, $(hash).offset().top);
+            });
+        }
+    });
+    $('#carouselExampleIndicators').carousel({
         keyboard: true,
         pause: false,
         ride: true
+    })
+
+    $('.news').carousel({
+        interval: false
     })
 });
 
