6b40961b9fe98977b16c115cc44f1844854fc4a0
Darqoo <d.smolag@gmail.com>
2018-02-11 17:49:36 +0100

    added slider structure

--- script/main.js
+++ script/main.js
@@ -1,3 +1,12 @@
+$(document).ready(function() {
+  
+    $('.carousel').carousel({
+        keyboard: true,
+        pause: false,
+        ride: true
+    })
+});
+
 $('body').click(function() {
     $('.collapse').collapse('hide');
 })
